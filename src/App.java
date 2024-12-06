1cd3d01d5218f6c569612e5cbdb5380b3233d19b
elberjsn <elberjsn@gmail.com>
2024-12-06 12:36:43 -0300

    refino

--- src/App.java
+++ src/App.java
@@ -4,27 +4,27 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Ola Seja bem vindo!");
 
-        Scanner nomeScanner = new Scanner(System.in);
-        System.out.println("Por favor, digite o Nome: ");
-        String nome = nomeScanner.nextLine();
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            System.out.println("Por favor, digite o Nome: ");
+            String nome = scanner.nextLine();
 
 
-        Scanner contaScanner = new Scanner(System.in);
-        System.out.println("Por favor, digite o número da Conta: ");
-        Integer conta = contaScanner.nextInt();
+            System.out.println("Por favor, digite o número da Conta: ");
+            Integer conta = scanner.nextInt();
 
 
-        Scanner agenciaScanner = new Scanner(System.in);
-        System.out.println("Por favor, digite o número da Agência ! ");
-        String agencia = agenciaScanner.nextLine();
+            System.out.println("Por favor, digite o número da Agência ! ");
+            String agencia = scanner.nextLine();
 
 
-        Scanner saldoScanner = new Scanner(System.in);
-        System.out.println("Por favor, digite o saldo inicial: ");
-        
-        Double saldo = saldoScanner.nextDouble();
+            System.out.println("Por favor, digite o saldo inicial: ");
+            Double saldo = scanner.nextDouble();
+            
 
-        System.out.println("Olá "+nome+", obrigado por criar uma conta em nosso banco,"
-                            +"sua agência é "+agencia+", conta "+conta+" e seu saldo "+saldo+" já está disponível para saque");
+            System.out.println("Olá "+nome+", obrigado por criar uma conta em nosso banco,"
+                                +"sua agência é "+agencia+", conta "+conta+" e seu saldo "+saldo+" já está disponível para saque");
+                                
+        }
     }
 }
